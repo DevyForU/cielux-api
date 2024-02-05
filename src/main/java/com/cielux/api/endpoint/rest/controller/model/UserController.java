065c53d1554ae41ac970f3772d968945e8c0c97c
FanomezanaNat <124127238+FanomezanaNat@users.noreply.github.com>
2024-02-05 18:12:26 +0300

    feat: add User class and associated components
    * chore:initialized project
    
    * chore: comment some action in workflow
    
    * feat: add user as model
    
    * feat: add user repository
    
    * feat: add user service
    
    * feat: add user controller
    
    * feat: update user controller

---
+++ src/main/java/com/cielux/api/endpoint/rest/controller/model/UserController.java
@@ -0,0 +1,44 @@
+package com.cielux.api.endpoint.rest.controller.model;
+
+import com.cielux.api.repository.model.User;
+import com.cielux.api.service.event.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.lang.String;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+    private UserService userService;
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser).getBody();
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable String id) throws ChangeSetPersister.NotFoundException {
+        return userService.getUserById(id);
+    }
+    @PutMapping("/user/{id}")
+    public User updateUser(@PathVariable String id,@RequestParam String name) throws ChangeSetPersister.NotFoundException {
+        return userService.updateUser(id,name);
+    }
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable String id){
+        userService.deleteUserById(id);
+    }
+
+}
