ff498e349aad99463486e329a3419e8956d5b741
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-02-08 21:54:04 +0300

    feat: add Cors configuration

--- src/main/java/com/cielux/api/PojaApplication.java
+++ src/main/java/com/cielux/api/PojaApplication.java
@@ -1,10 +1,14 @@
 package com.cielux.api;
 
+import com.cielux.api.repository.conf.CorsConf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @PojaGenerated
+@Import(CorsConf.class)
+
 public class PojaApplication {
 
   public static void main(String[] args) {
